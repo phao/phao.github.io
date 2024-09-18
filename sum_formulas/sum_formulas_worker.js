66a9c04170204060f3974ed9518874df1c6c0b19
Pedro Henrique Antunes de Oliveira <phao1989@gmail.com>
2024-09-18 13:09:24 -0300

    Update sum_formulas_worker.js

--- sum_formulas/sum_formulas_worker.js
+++ sum_formulas/sum_formulas_worker.js
@@ -2,5 +2,5 @@ importScripts("BigInt_BigRat.min.js", "sum_formulas.js");
 
 onmessage = function (msg) {
   p = msg.data;
-  postMessage(pol_latex(sum_formula(p), "n"));
+  postMessage(pol_latex(sum_formula_rec(p), "n"));
 }
